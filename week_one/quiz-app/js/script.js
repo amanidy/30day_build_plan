98c0e286e2f3614ce98f173135cc0ed4ba667163
amanidy <amaniarnold08@gmail.com>
2026-03-08 19:08:10 +0300

    Hard coded questions in quiz app

---
+++ week_one/quiz-app/js/script.js
@@ -0,0 +1,15 @@
+const questions = [
+  {
+    question:"What is your name?",
+    answers:['Felix','Don','Mutinda','Kamau']
+  },
+    {
+    question:"How old are you?",
+    answers:['8','20','32','25']
+  },
+    {
+    question:"What is your favorite city?",
+    answers:['Nyeri','Nairobi','Mombasa','Nakuru']
+  }
+  
+]
